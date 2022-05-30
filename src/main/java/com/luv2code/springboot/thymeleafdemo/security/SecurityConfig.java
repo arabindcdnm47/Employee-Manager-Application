999a5de84056252e1ed00e5ab305fd4552915aac
Arabind Sandrappan <arabindcdnm47@gmail.com>
2022-05-30 11:38:49 +0530

    Initial commit

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/security/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.luv2code.springboot.thymeleafdemo.security;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+	
+	
+}
---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.luv2code.springboot.thymeleafdemo.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
